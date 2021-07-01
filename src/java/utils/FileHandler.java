2be8204e9fc9bf8ecad86cade80079adfc17a3be
Csaba79-coder <csabavadasz79@gmail.com>
2021-07-01 10:34:16 +0200

    Initial commit

---
+++ src/java/utils/FileHandler.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class FileHandler {
+    /**
+     * FileHandler class should be used to uplaod data from files
+     *
+     * hint: String split() method
+     */
+    private final String sourceDirectory = "src/resources";
+}
